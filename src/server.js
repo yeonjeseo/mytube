72b279fb5a4b154cf4eff5f7e40f153adee65ae9
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-08-09 15:34:39 +0900

    HTTP methods : GET (request, response)

--- src/server.js
+++ src/server.js
@@ -4,8 +4,16 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const handleListening = () => {
-  console.log(`Server listening on port httpL//localhost:${PORT}😀`);
+const handleHome = (request, response) => {
+  response.send(`<h1>some html?</h1>`);
 };
+app.get("/", handleHome);
+const handleLogin = (req, res) => {
+  return res.send({ message: "login" });
+};
+app.get("/login", handleLogin);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}😀`);
 
 app.listen(PORT, handleListening);
