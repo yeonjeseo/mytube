c9a3a0304a996660738901577e551c9774ffab71
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-08-12 17:56:56 +0900

    Component reuse : pug Mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,37 @@
-const fakeUser = {
-  userName: "Jeffrey",
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  const obj = {
+    name: "Jeffrey",
+    age: 32,
+    major: "Aerospace Engineering",
+  };
+  return res.render("home", { pageTitle: "Home", videos, obj });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => {
   return res.render("watch", { pageTitle: "Watch", potato: "potato" });
 };
