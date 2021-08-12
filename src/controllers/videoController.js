2879455b22104dcbb385564f6cd02ee8e9e7447c
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-08-12 19:42:21 +0900

    Use express req.params

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,42 +1,44 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ];
-  const obj = {
-    name: "Jeffrey",
-    age: 32,
-    major: "Aerospace Engineering",
-  };
-  return res.render("home", { pageTitle: "Home", videos, obj });
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const see = (req, res) => {
-  return res.render("watch", { pageTitle: "Watch", potato: "potato" });
+  //ES6 way
+  const { id } = req.params;
+  // const id = req.params.id;
+  //get id from express req.params
+  //then find the video that matchs the id
+  const video = videos[id - 1];
+
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
 };
 export const edit = (req, res) => {
-  console.log(req.params);
   return res.render("edit");
 };
 export const search = (req, res) => res.send("Search Video");
