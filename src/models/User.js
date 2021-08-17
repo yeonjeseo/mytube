d7f8f8a5dce674462e474c82a1116e2de228fc7f
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-08-17 14:32:42 +0900

    Encrypt password using hash functio(bcrypt.js)

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,18 @@
 import mongoose from "mongoose";
+import bcript from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
   location: String,
+});
+
+userSchema.pre("save", async function () {
+  console.log(this.password);
+  this.password = await bcript.hash(this.password, 5);
+  console.log(this.password);
 });
 
 const User = mongoose.model("User", userSchema);
