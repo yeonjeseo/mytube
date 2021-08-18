af41bf394656e1faeaea3a135716804a6f9bb1d1
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-08-18 21:26:10 +0900

    Session explanation

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import globalRouter from "./routers/rootRouter";
 import usersRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import session from "express-session";
 
 const app = express();
 const logger = morgan("dev");
@@ -14,8 +15,28 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 // let express uderstands and transforms the form values into javascript
 app.use(express.urlencoded({ extended: true }));
-
 app.use(logger);
+
+app.use(
+  session({
+    secret: "Hello",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id}\n${req.session.potato}`);
+});
+
 app.use("/", globalRouter);
 app.use("/users", usersRouter);
 app.use("/videos", videoRouter);
