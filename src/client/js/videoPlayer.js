3743ec9ec6c17fbf9e766cbb7f1807e1a29596c1
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-08-26 18:35:02 +0900

    Video time formatting, full screen

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,10 +1,12 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volume = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const duration = document.getElementById("duration");
+const timeline = document.getElementById("timeline");
+const fullscreenBtn = document.getElementById("fullscreen");
+const videoContainer = document.getElementById("videoContainer");
 
 // global variables
 video.volume = 0.5;
@@ -37,7 +39,6 @@ const handleMute = () => {
     volume.value = "0";
     video.muted = true;
   }
-
   muteBtn.textContent = video.muted ? "Unmute" : "Mute";
 };
 
@@ -53,9 +54,48 @@ const handleVolume = (event) => {
   video.volume = value;
 };
 
+const timeFormatter = (seconds) => {
+  return new Date(seconds * 1000).toISOString().slice(11, 19);
+};
+
 //display total duration
+const handleLoadedMetadata = () => {
+  totalDuration = Math.floor(video.duration);
+  duration.textContent = timeFormatter(totalDuration);
+  timeline.max = totalDuration.toString();
+  console.log(timeline.max);
+};
+
+const handleTimeUpdate = () => {
+  //current time change
+  currentTime.textContent = timeFormatter(Math.floor(video.currentTime));
+  //change input range
+  timeline.value = video.currentTime;
+};
+
+// handle timeline
+const handleTimeline = (event) => {
+  video.currentTime = event.target.value;
+};
+
+const handleFullScreen = () => {
+  if (!document.fullscreenElement) {
+    videoContainer.requestFullscreen();
+    fullscreenBtn.innerText = "Exit Full Screen";
+  } else {
+    document.exitFullscreen();
+    fullscreenBtn.innerText = "Enter Full Screen";
+  }
+};
 
 // define event listener
 playBtn.addEventListener("click", handlePlay);
 muteBtn.addEventListener("click", handleMute);
 volume.addEventListener("input", handleVolume);
+timeline.addEventListener("input", handleTimeline);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+fullscreenBtn.addEventListener("click", handleFullScreen);
+
+//fire interval for callback
+// setInterval(handleCurrentTime, 1000);
